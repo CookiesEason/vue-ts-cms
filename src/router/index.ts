50dd8a1f96823ab6762d07b2d444da91f7e93a22
xzy <837447352@qq.com>
2022-05-27 11:55:55 +0800

    feat: init project

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,18 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHashHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
 
-const routes: Array<RouteRecordRaw> = [
+const routes: RouteRecordRaw[] = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    redirect: '/login',
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: '/login',
+    component: () => import('@/views/login/LoginView.vue'),
+  },
+  {
+    path: '/main',
+    component: () => import('@/views/main/MainView.vue'),
   },
 ];
 
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex';
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+const store = createStore({
+  state: () => {
+    return {
+      name: 'xxxxxxxx',
+    };
+  },
 });
+
+export default store;
